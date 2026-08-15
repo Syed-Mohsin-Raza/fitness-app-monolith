92d5ba316220e32c8ff6ef8aa0ccfbc230aaf2f7
Syed-Mohsin-Raza <raza.mohsin1441@gmail.com>
2026-08-15 13:15:01 +0500

    feat(model): define JPA relationships between entities

--- src/main/java/com/project/fitness/model/Recommendation.java
+++ src/main/java/com/project/fitness/model/Recommendation.java
@@ -1,5 +1,6 @@
 package com.project.fitness.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
@@ -12,6 +13,16 @@ public class Recommendation {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(name = "fk_recommendation_user"))
+    @JsonIgnore
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "activity_id", nullable = false, foreignKey = @ForeignKey(name = "fk_recommendation_activity"))
+    @JsonIgnore
+    private Activity activity;
 
     private String type;
 

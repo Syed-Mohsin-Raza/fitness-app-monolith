20fa8ae298787eb86c826816a095158c1f040da3
Syed-Mohsin-Raza <raza.mohsin1441@gmail.com>
2026-08-19 22:57:02 +0500

    feat(recommendations): add endpoint to fetch recommendations by user ID

--- src/main/java/com/project/fitness/controller/RecommendationController.java
+++ src/main/java/com/project/fitness/controller/RecommendationController.java
@@ -5,10 +5,9 @@ import com.project.fitness.model.Recommendation;
 import com.project.fitness.service.RecommendationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/recommendation")
@@ -22,5 +21,14 @@ public class RecommendationController {
             @RequestBody RecommendationRequest request) {
         Recommendation recommendation = recommendationService.generateRecommendation(request);
         return ResponseEntity.ok(recommendation);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Recommendation>> getUserRecommendation(
+            @PathVariable String userId
+    ) {
+        List<Recommendation> recommendationList
+                = recommendationService.getUserRecommendation(userId);
+        return ResponseEntity.ok(recommendationList);
     }
 }
